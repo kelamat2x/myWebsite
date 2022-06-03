3e04f3c78686392e5ebccb665dc232e08136e020
blogamthuc <o0othanh2k1@gmail.com>
2022-06-03 09:24:42 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Topbar from "./components/topbar/Topbar";
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  const currentUser = true;
+  return (
+    <Router>
+      <Topbar />
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+
+        <Route path="/posts">
+          <Homepage />
+        </Route>
+
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write">{currentUser ? <Write /> : <Write />}</Route>
+        <Route path="/login">{currentUser ? <Login /> : <Login />}</Route>
+        <Route path="/register">{currentUser ? <Register /> : <Register />}</Route>
+
+        <Route path="/settings">
+          {currentUser ? <Settings /> : <Login />}
+        </Route>
+        
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
